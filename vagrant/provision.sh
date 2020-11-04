0071bed3e3b078af48b4edc5349ac41de41dec74
startinmerc <startinmerc@gmail.com>
2020-11-04 15:43:40 +0000

    Initial commit

---
+++ vagrant/provision.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+PROJECT_NAME=$1
+
+PROJECT_DIR=/vagrant
+VIRTUALENV_DIR=/home/vagrant/.virtualenvs/$PROJECT_NAME
+
+PYTHON=$VIRTUALENV_DIR/bin/python
+PIP=$VIRTUALENV_DIR/bin/pip
+
+
+# Create database
+su - postgres -c "createuser vagrant -d"
+su - vagrant -c "createdb $PROJECT_NAME"
+
+
+# Virtualenv setup for project
+su - vagrant -c "python3.8 -m venv $VIRTUALENV_DIR"
+
+su - vagrant -c "echo $PROJECT_DIR > $VIRTUALENV_DIR/.project"
+
+
+# Upgrade PIP itself
+su - vagrant -c "$PIP install --upgrade pip"
+
+# Upgrade setuptools (for example html5lib needs 1.8.5+)
+su - vagrant -c "$PIP install --upgrade six setuptools"
+
+# Install PIP requirements
+su - vagrant -c "$PIP install -r $PROJECT_DIR/requirements.txt"
+
+
+# Set execute permissions on manage.py as they get lost if we build from a zip file
+chmod a+x $PROJECT_DIR/manage.py
+
+
+# running migrations here is typically not necessary because of fab pull_data
+# su - vagrant -c "$PYTHON $PROJECT_DIR/manage.py migrate --noinput"
+
+
+# Add a couple of aliases to manage.py into .bashrc
+cat << EOF >> /home/vagrant/.bashrc
+export PYTHONPATH=$PROJECT_DIR
+export DJANGO_SETTINGS_MODULE=$PROJECT_NAME.settings.dev
+alias dj="django-admin.py"
+alias djrun="dj runserver 0.0.0.0:8000"
+alias djrunp="dj runserver_plus 0.0.0.0:8000"
+source $VIRTUALENV_DIR/bin/activate
+export PS1="[$PROJECT_NAME \W]\\$ "
+cd $PROJECT_DIR
+EOF
