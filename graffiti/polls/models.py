a844a5e00db092ea9bcdfd3cb0af8a3573ecd003
startinmerc <startinmerc@gmail.com>
2020-11-05 10:28:10 +0000

    Typo o_O

--- graffiti/polls/models.py
+++ graffiti/polls/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Question(models.Model):
   question_text = models.CharField(max_length=200)
-  pub_date = models.DataTimeField('date published')
+  pub_date = models.DateTimeField('date published')
 
 class Choice(models.Model):
   question = models.ForeignKey(Question, on_delete=models.CASCADE)
