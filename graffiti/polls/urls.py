5cd33641322e2437603ecc730a08601cbe3db4b9
startinmerc <startinmerc@gmail.com>
2020-11-05 17:29:20 +0000

    (Tutorial) Convert polls app views to REST

--- graffiti/polls/urls.py
+++ graffiti/polls/urls.py
@@ -3,12 +3,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # /polls/
-    path("", views.index, name="index"),
-    # /polls/5/
-    path("<int:question_id>/", views.detail, name='detail'),
-    # /polls/5/results/
-    path("<int:question_id>/results/", views.results, name="results"),
-    # /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    ## /polls/
+    # path("", views.index, name="index"),
+    ## /polls/5/
+    # path("<int:question_id>/", views.detail, name='detail'),
+    ##/polls/5/results/
+    # path("<int:question_id>/results/", views.results, name="results"),
+    ## /polls/5/vote/
+    # path('<int:question_id>/vote/', views.vote, name='vote'),
+    ## ===
+    path('', views.question_list),
+    path('<int:pk>/', views.question_detail),
 ]
