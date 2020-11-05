1bfd5edd62325a8ba197db93c4cf5377c871047c
startinmerc <startinmerc@gmail.com>
2020-11-05 17:28:32 +0000

    include artworks urls

--- graffiti/graffiti/urls.py
+++ graffiti/graffiti/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
+    path('artworks/', include('artworks.urls')),
     path('admin/', admin.site.urls),
 ]
