d3f8fcf0d502ae879a3bef7ccd2c3e505ced3b8a
startinmerc <startinmerc@gmail.com>
2020-11-05 17:50:10 +0000

    Refactor views to use rest_framework Response

--- graffiti/artworks/views.py
+++ graffiti/artworks/views.py
@@ -1,92 +1,79 @@
-from django.http import HttpResponse, JsonResponse
-from rest_framework.parsers import JSONParser
-from .serializers import ArtistSerializer, ArtworkSerializer
-# ===FOR DEV
-from django.views.decorators.csrf import csrf_exempt
-# ===
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from .models import Artist, Artwork
+from .serializers import ArtistSerializer, ArtworkSerializer
 
-# ===FOR DEV
-@csrf_exempt
-# ===
+@api_view(['GET','POST'])
 def artist_list(request):
+  # List all artists
   if request.method == 'GET':
     artists = Artist.objects.all()
     serializer = ArtistSerializer(artists, many=True)
-    return JsonResponse(serializer.data, safe=False)
-
+    return Response(serializer.data)
+  # Post new artist
   elif request.method == 'POST':
-    data = JSONParser().parse(request)
-    serializer = ArtistSerializer(data=data)
+    serializer = ArtistSerializer(data=request.data)
     if serializer.is_valid():
       serializer.save()
-      return JsonResponse(serializer.data, status=201)
-    return JsonResponse(serializer.errors, status=400)
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# ===FOR DEV
-@csrf_exempt
-# ===
+@api_view(['GET','PUT','DELETE'])
 def artist_detail(request, pk):
   try:
     artist = Artist.objects.get(pk=pk)
   except Artist.DoesNotExist:
-    return HttpResponse(status=404)
+    return Response(status=status.HTTP_404_NOT_FOUND)
 
   if request.method == 'GET':
     serializer = ArtistSerializer(artist)
-    return JsonResponse(serializer.data)
+    return Response(serializer.data)
 
   elif request.method == 'PUT':
-    data = JSONParser().parse(request)
-    serializer = ArtistSerializer(artist, data=data)
+    serializer = ArtistSerializer(artist, data=request.data)
     if serializer.is_valid():
       serializer.save()
-      return JsonResponse(serializer.data)
-    return JsonResponse(serializer.errors, status=404)
+      return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
   
   elif request.method == 'DELETE':
     artist.delete()
-    return HttpResponse(status=204)
+    return Response(status=status.HTTP_204_NO_CONTENT)
 
-# ===FOR DEV
-@csrf_exempt
-# ===
+@api_view(['GET', 'POST'])
 def artwork_list(request):
-  if request.methon == 'GET':
+  if request.method == 'GET':
     artworks = Artwork.objects.all()
     serializer = ArtworkSerializer(artworks, many=True)
-    return JsonResponse(serializer.data, safe=False)
+    return Response(serializer.data)
 
   elif request.method == 'POST':
-    data = JSONParser().parse(request)
-    serializer = ArtworkSerializer(data=data)
+    serializer = ArtworkSerializer(data=request.data)
     if serializer.is_valid():
       serializer.save()
-      return JsonResponse(serializer.data, status=201)
-    return JsonResponse(serializer.errors, status=400)
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# ===FOR DEV
-@csrf_exempt
-# ===
+@api_view(['GET','PUT','DELETE'])
 def artwork_detail(request, pk):
   try:
     artwork = Artwork.objects.get(pk=pk)
   except Artwork.DoesNotExist:
-    return HttpResponse(status=404)
+    return Response(status=status.HTTP_404_NOT_FOUND)
 
   if request.method == 'GET':
     serializer = ArtworkSerializer(artwork)
-    return JsonResponse(serializer.data)
+    return Response(serializer.data)
 
   elif request.method == 'PUT':
-    data = JSONParser().parse(request)
-    serializer = ArtworkSerializer(artwork, data=data)
+    serializer = ArtworkSerializer(artwork, data=request.data)
     if serializer.is_valid():
       serializer.save()
-      return JsonResponse(serializer.data)
-    return JsonResponse(serializer.errors, status=404)
+      return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
   
   elif request.method == 'DELETE':
     artwork.delete()
-    return HttpResponse(status=204)
+    return Response(status=status.HTTP_204_NO_CONTENT)
