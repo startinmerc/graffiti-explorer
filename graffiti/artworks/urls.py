92fc5e31e7734db7367d54d6f0cdb8c4c3b1214b
startinmerc <startinmerc@gmail.com>
2020-11-05 18:34:18 +0000

    Refactor views as classes

--- graffiti/artworks/urls.py
+++ graffiti/artworks/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('artists/', views.artist_list),
-    path('artists/<int:pk>/', views.artist_detail),
+    path('artists/', views.ArtistList.as_view()),
+    path('artists/<int:pk>/', views.ArtistDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- graffiti/artworks/views.py
+++ graffiti/artworks/views.py
@@ -1,79 +1,47 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
+from django.http import Http404
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Artist, Artwork
 from .serializers import ArtistSerializer, ArtworkSerializer
 
-@api_view(['GET','POST'])
-def artist_list(request, format=None):
+class ArtistList(APIView):
   # List all artists
-  if request.method == 'GET':
+  def get(self, request, format=None):
     artists = Artist.objects.all()
     serializer = ArtistSerializer(artists, many=True)
     return Response(serializer.data)
   # Post new artist
-  elif request.method == 'POST':
+  def post(self, request, format=None):
     serializer = ArtistSerializer(data=request.data)
     if serializer.is_valid():
       serializer.save()
       return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET','PUT','DELETE'])
-def artist_detail(request, pk, format=None):
-  try:
-    artist = Artist.objects.get(pk=pk)
-  except Artist.DoesNotExist:
-    return Response(status=status.HTTP_404_NOT_FOUND)
-
-  if request.method == 'GET':
+class ArtistDetail(APIView):
+  # Find artist
+  def get_object(self,pk):
+    try:
+      return Artist.objects.get(pk=pk)
+    except Artist.DoesNotExist:
+      raise Http404
+  
+  def get(self, request, pk, format=None):
+    artist = self.get_object(pk)
     serializer = ArtistSerializer(artist)
     return Response(serializer.data)
 
-  elif request.method == 'PUT':
+  def put(self, request, pk, format=None):
+    artist = self.get_object(pk)
     serializer = ArtistSerializer(artist, data=request.data)
     if serializer.is_valid():
       serializer.save()
       return Response(serializer.data)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
   
-  elif request.method == 'DELETE':
+  def delete(self, request, pk, format=None):
+    artist = self.get_object(pk)
     artist.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
-
-@api_view(['GET', 'POST'])
-def artwork_list(request, format=None):
-  if request.method == 'GET':
-    artworks = Artwork.objects.all()
-    serializer = ArtworkSerializer(artworks, many=True)
-    return Response(serializer.data)
-
-  elif request.method == 'POST':
-    serializer = ArtworkSerializer(data=request.data)
-    if serializer.is_valid():
-      serializer.save()
-      return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET','PUT','DELETE'])
-def artwork_detail(request, pk, format=None):
-  try:
-    artwork = Artwork.objects.get(pk=pk)
-  except Artwork.DoesNotExist:
-    return Response(status=status.HTTP_404_NOT_FOUND)
-
-  if request.method == 'GET':
-    serializer = ArtworkSerializer(artwork)
-    return Response(serializer.data)
-
-  elif request.method == 'PUT':
-    serializer = ArtworkSerializer(artwork, data=request.data)
-    if serializer.is_valid():
-      serializer.save()
-      return Response(serializer.data)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-  
-  elif request.method == 'DELETE':
-    artwork.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
