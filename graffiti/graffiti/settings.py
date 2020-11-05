80ebdd229c5b9b79cd7fb34cd71327d188919c3f
startinmerc <startinmerc@gmail.com>
2020-11-05 17:27:05 +0000

    Update installed apps

--- graffiti/graffiti/settings.py
+++ graffiti/graffiti/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
+    'artworks.apps.ArtworksConfig',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
