c92036e76bdae80265886303cb9a893c65a820b1
startinmerc <startinmerc@gmail.com>
2020-11-05 18:40:22 +0000

    Biiiig refactor to use rest_fraameworks generics

--- graffiti/artworks/views.py
+++ graffiti/artworks/views.py
@@ -1,47 +1,21 @@
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-
 from .models import Artist, Artwork
 from .serializers import ArtistSerializer, ArtworkSerializer
+from rest_framework import generics
 
-class ArtistList(APIView):
-  # List all artists
-  def get(self, request, format=None):
-    artists = Artist.objects.all()
-    serializer = ArtistSerializer(artists, many=True)
-    return Response(serializer.data)
-  # Post new artist
-  def post(self, request, format=None):
-    serializer = ArtistSerializer(data=request.data)
-    if serializer.is_valid():
-      serializer.save()
-      return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class ArtistList(generics.ListCreateAPIView):
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
 
-class ArtistDetail(APIView):
-  # Find artist
-  def get_object(self,pk):
-    try:
-      return Artist.objects.get(pk=pk)
-    except Artist.DoesNotExist:
-      raise Http404
-  
-  def get(self, request, pk, format=None):
-    artist = self.get_object(pk)
-    serializer = ArtistSerializer(artist)
-    return Response(serializer.data)
 
-  def put(self, request, pk, format=None):
-    artist = self.get_object(pk)
-    serializer = ArtistSerializer(artist, data=request.data)
-    if serializer.is_valid():
-      serializer.save()
-      return Response(serializer.data)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-  
-  def delete(self, request, pk, format=None):
-    artist = self.get_object(pk)
-    artist.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
+class ArtistDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
+    
+class ArtworkList(generics.ListCreateAPIView):
+    queryset = Artwork.objects.all()
+    serializer_class = ArtworkSerializer
+
+
+class ArtworkDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Artwork.objects.all()
+    serializer_class = ArtworkSerializer
