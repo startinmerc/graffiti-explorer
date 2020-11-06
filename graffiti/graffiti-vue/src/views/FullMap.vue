e27cdb4c56363d9786c18aa40d761d1321cd6bf4
startinmerc <startinmerc@gmail.com>
2020-11-06 15:01:47 +0000

    Add mapbox-gl scripting to FullMap

--- graffiti/graffiti-vue/src/views/FullMap.vue
+++ graffiti/graffiti-vue/src/views/FullMap.vue
@@ -1,18 +1,36 @@
 <template>
 	<main>
-		<h1>Map goes here.</h1>
+		<div id="map-container"></div>
 	</main>
 </template>
 
 <script>
+import mapboxgl from "mapbox-gl";
+
 export default {
-  name: "FullMap"
+	name: "FullMap",
+
+	mounted() {
+		mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_TOKEN;
+
+		var map = new mapboxgl.Map({
+			container: "map-container",
+			style: "mapbox://styles/startinmerc/ckh3c2oam2izs19nknv2xyviy",
+			center: [-1.07825, 53.95798],
+			zoom: 13,
+		});
+
+		// add markers...
+	},
 };
 </script>
 
-<style scoped lang="scss">
-main {
-	display: grid;
-	place-items: center;
+<style scoped>
+.marker {
+	margin: 0;
+}
+#map-container {
+	width: 100%;
+	height: 100vh;
 }
 </style>
