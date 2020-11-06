f5ee8eab3d8a0459b8d40e710ff30339d202d204
startinmerc <startinmerc@gmail.com>
2020-11-06 12:26:36 +0000

    Add menu component

---
+++ graffiti/graffiti-vue/src/components/Menu.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav>
+    <b>York<br />Graffiti<br />Explorer</b>
+    <button>menu</button>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Menu"
+}
+</script>
+
+<style lang="scss">
+nav {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: calc(100% - 2rem);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  background-color: var(--darkblue);
+  color: var(--red);
+  ul {
+    position: absolute;
+  }
+  button {
+    border: 1px solid var(--red);
+  }
+}
+</style>
