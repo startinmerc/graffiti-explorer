ad6e43c94a7ef5e7ddbff5eb5c8abceabc360181
startinmerc <startinmerc@gmail.com>
2020-11-06 11:48:32 +0000

    Prettify

--- graffiti/graffiti-vue/src/App.vue
+++ graffiti/graffiti-vue/src/App.vue
@@ -17,32 +17,35 @@ export default {
 
 <style>
 :root {
-  --darkblue: #33405C;
-  --lightblue: #AFDEE4;
-  --yellow: #FEF05B;
-  --red: #E14E6D;
-  --black: #221E1D;
-  --white: #F9F9F9;
+	--darkblue: #33405c;
+	--lightblue: #afdee4;
+	--yellow: #fef05b;
+	--red: #e14e6d;
+	--black: #221e1d;
+	--white: #f9f9f9;
 }
 
 /* Style resets */
 body {
 	font-family: "Raleway", sans-serif;
-  margin: 0;
+	margin: 0;
+}
+#app {
+  overflow: hidden;
+  max-width: 100vw;
 }
 button {
-  border: 0;
-  border-radius: 500rem;
-  background: var(--darkblue);
-  color: var(--red);
-  font-family: inherit;
-  font-size: inherit;
-  line-height: 1.2;
-  white-space: nowrap;
-  text-decoration: none;
-  padding: 0.25rem 0.5rem;
-  margin: 0.25rem;
-  cursor: pointer;
+	border: 0;
+	border-radius: 500rem;
+	background: var(--darkblue);
+	color: var(--red);
+	font-family: inherit;
+	font-size: inherit;
+	line-height: 1.2;
+	white-space: nowrap;
+	text-decoration: none;
+	padding: 0.25rem 0.5rem;
+	margin: 0.25rem;
+	cursor: pointer;
 }
-
 </style>
