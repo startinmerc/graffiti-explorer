e4421fe52839202b898f005d4463a6907aac5298
startinmerc <startinmerc@gmail.com>
2020-11-06 13:00:27 +0000

    Add template pages

---
+++ graffiti/graffiti-vue/src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+	<main>
+		<h1>about</h1>
+	</main>
+</template>
+
+<script>
+export default {
+	name: "About",
+};
+</script>
+
+<style scoped lang="scss">
+main {
+	display: grid;
+	place-items: center;
+}
+</style>
---
+++ graffiti/graffiti-vue/src/views/FullMap.vue
@@ -0,0 +1,18 @@
+<template>
+	<main>
+		<h1>Map goes here.</h1>
+	</main>
+</template>
+
+<script>
+export default {
+  name: "FullMap"
+};
+</script>
+
+<style scoped lang="scss">
+main {
+	display: grid;
+	place-items: center;
+}
+</style>
