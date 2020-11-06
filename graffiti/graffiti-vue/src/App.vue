9cb372809ef027476a2e813f22f69ccbe22ec6b8
startinmerc <startinmerc@gmail.com>
2020-11-06 11:12:52 +0000

    Remove defaults, add Homepage

--- graffiti/graffiti-vue/src/App.vue
+++ graffiti/graffiti-vue/src/App.vue
@@ -1,19 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<Homepage />
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Homepage from "./components/Homepage.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+	components: {
+		Homepage,
+	},
+};
 </script>
 
 <style>
---
+++ graffiti/graffiti-vue/src/components/Homepage.vue
@@ -0,0 +1,32 @@
+<template>
+	<main>
+		<div>
+			<h1>York<br />Graffiti<br />Explorer</h1>
+			<button>View the map</button>
+		</div>
+	</main>
+</template>
+
+<script>
+export default {
+	name: "Homepage",
+};
+</script>
+
+<style scoped lang="scss">
+main {
+	height: 100vh;
+	background: pink;
+	text-align: center;
+	color: var(--red);
+	h1 {
+		font-size: 4rem;
+		background: var(--darkblue);
+		padding: 2rem;
+		display: inline-block;
+		margin-top: 15vh;
+		margin-bottom: 10vh;
+		text-align: left;
+	}
+}
+</style>
