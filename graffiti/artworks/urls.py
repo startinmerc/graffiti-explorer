5b91c09d67a2cd7f9122cc9635bc68f9e7a8a032
startinmerc <startinmerc@gmail.com>
2020-11-06 10:05:13 +0000

    Add artworks urls

--- graffiti/artworks/urls.py
+++ graffiti/artworks/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('artists/', views.ArtistList.as_view()),
     path('artists/<int:pk>/', views.ArtistDetail.as_view()),
+    path('artworks/', views.ArtworkList.as_view()),
+    path('artworks/<int:pk>/', views.ArtworkDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
