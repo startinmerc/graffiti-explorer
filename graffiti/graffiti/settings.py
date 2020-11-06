82ace04dfddec9cdfdf0925f38575810ad9d096c
startinmerc <startinmerc@gmail.com>
2020-11-06 10:04:50 +0000

    Remove polls tutorial app

--- graffiti/graffiti/settings.py
+++ graffiti/graffiti/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
     'artworks.apps.ArtworksConfig',
     'rest_framework',
     'django.contrib.admin',
--- graffiti/graffiti/urls.py
+++ graffiti/graffiti/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
     path('artworks/', include('artworks.urls')),
     path('admin/', admin.site.urls),
 ]
