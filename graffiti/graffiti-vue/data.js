395acdfc0e536107afda1a01f05edbb46e1db46c
startinmerc <startinmerc@gmail.com>
2020-11-09 11:58:03 +0000

    Add description to testData

--- graffiti/graffiti-vue/data.js
+++ graffiti/graffiti-vue/data.js
@@ -1,9 +1,39 @@
 export const testData = [
-	{ title: "test data 1", location: [-1.077706, 53.956799] },
-	{ title: "test data 2", location: [-1.086944, 53.957329] },
-	{ title: "test data 3", location: [-1.088489, 53.959355] },
-	{ title: "test data 4", location: [-1.087631, 53.96402] },
-	{ title: "test data 5", location: [-1.082696, 53.964108] },
+	{
+		title: "test data 1",
+		location: [-1.077706, 53.956799],
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex in illo quasi, repellat soluta porro architecto? Facilis distinctio natus iste illum quaerat provident debitis, numquam molestiae! Fuga similique officiis laboriosam!",
+		artist: "",
+	},
+	{
+		title: "test data 2",
+		location: [-1.086944, 53.957329],
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex in illo quasi, repellat soluta porro architecto? Facilis distinctio natus iste illum quaerat provident debitis, numquam molestiae! Fuga similique officiis laboriosam!",
+		artist: "",
+	},
+	{
+		title: "test data 3",
+		location: [-1.088489, 53.959355],
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex in illo quasi, repellat soluta porro architecto? Facilis distinctio natus iste illum quaerat provident debitis, numquam molestiae! Fuga similique officiis laboriosam!",
+		artist: "",
+	},
+	{
+		title: "test data 4",
+		location: [-1.087631, 53.96402],
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex in illo quasi, repellat soluta porro architecto? Facilis distinctio natus iste illum quaerat provident debitis, numquam molestiae! Fuga similique officiis laboriosam!",
+		artist: "",
+	},
+	{
+		title: "test data 5",
+		location: [-1.082696, 53.964108],
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex in illo quasi, repellat soluta porro architecto? Facilis distinctio natus iste illum quaerat provident debitis, numquam molestiae! Fuga similique officiis laboriosam!",
+		artist: "",
+	},
 ];
 
 function buildJSON(data) {
@@ -14,7 +44,8 @@ function buildJSON(data) {
 			coordinates: v.location,
 		},
 		properties: {
-			name: v.title,
+			title: v.title,
+			description: v.description,
 			icon: "rocket",
 		},
 	}));
