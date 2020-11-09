0c87f3a22fd69be784a6ae6c8d83490d657b267d
startinmerc <startinmerc@gmail.com>
2020-11-09 10:57:53 +0000

    Move loadMap to Vue method, add addPoints and addLayer methods

--- graffiti/graffiti-vue/src/views/FullMap.vue
+++ graffiti/graffiti-vue/src/views/FullMap.vue
@@ -12,16 +12,53 @@ export default {
 	name: "FullMap",
 
 	mounted() {
+		// Get API token from .env
 		mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_TOKEN;
 
-		var map = new mapboxgl.Map({
-			container: "map-container",
-			style: "mapbox://styles/startinmerc/ckh3c2oam2izs19nknv2xyviy",
-			center: [-1.07825, 53.95798],
-			zoom: 13,
+		// Load map
+		this.loadMap().then((mapBox) => {
+			// Add points
+			this.addPoints(mapBox);
+			// Add layer with points
+			this.addLayer(mapBox);
 		});
+	},
+	methods: {
+		loadMap() {
+			// Async loading function to await styles
+			return new Promise((resolve) => {
+				let mapBox = new mapboxgl.Map({
+					container: "map-container",
+					// Custom mapbox style
+					style: "mapbox://styles/startinmerc/ckh3c2oam2izs19nknv2xyviy",
+					// Arbitrary, to change to auto-fit points
+					center: [-1.07825, 53.95798],
+					// Arbitrary, to change to auto-fit points
+					zoom: 13,
+				});
+				mapBox.on("styledata", () => {
+					resolve(mapBox);
+				});
+			});
+		},
+		addPoints(mapBox) {
+			// Import geoJSON
+			mapBox.addSource("places", geoData);
+		},
+		addLayer(mapBox) {
+			mapBox.addLayer({
+				id: "places",
+				type: "symbol",
+				source: "places",
+				layout: {
+					// To change to custom eye marker
+					"icon-image": "{icon}-15",
+					"icon-allow-overlap": true,
+				},
+			});
+		},
 
-		// add markers...
+		},
 	},
 };
 </script>
