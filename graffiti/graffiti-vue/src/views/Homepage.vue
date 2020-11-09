258ca39a41513813f6ebc01ed85a3c34314b9a1f
startinmerc <startinmerc@gmail.com>
2020-11-09 15:45:25 +0000

    Include arrow icon in Homepage button

--- graffiti/graffiti-vue/src/views/Homepage.vue
+++ graffiti/graffiti-vue/src/views/Homepage.vue
@@ -1,15 +1,32 @@
 <template>
 	<main>
 		<div>
-			<h1>York<br />Graffiti<br />Explorer</h1>
-			<router-link to="/map"><button>View the map</button></router-link>
+			<h1>
+				York<br />
+				Graffiti<br />
+				Explorer
+			</h1>
+			<router-link to="/map">
+				<button>
+					View the map
+					<icon-base icon-name="arrow-right">
+						<ArrowRight />
+					</icon-base>
+				</button>
+			</router-link>
 		</div>
 	</main>
 </template>
 
 <script>
+import IconBase from "../components/icons/IconBase";
+import ArrowRight from "../components/icons/ArrowRight";
 export default {
 	name: "Homepage",
+	components: {
+		IconBase,
+		ArrowRight,
+	},
 };
 </script>
 
