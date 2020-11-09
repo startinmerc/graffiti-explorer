d2f315010cbe19f3d87c0c61f1836edc61cc357b
startinmerc <startinmerc@gmail.com>
2020-11-09 16:10:05 +0000

    Add ArrowRight to ArtworkPopup

--- graffiti/graffiti-vue/src/components/ArtworkPopup.vue
+++ graffiti/graffiti-vue/src/components/ArtworkPopup.vue
@@ -3,15 +3,26 @@
 		<div class="placeholder"></div>
 		<h3>{{ title }}</h3>
 		<p>Artist</p>
-		<button>View Detail</button>
+		<button>
+			View Detail
+			<icon-base icon-name="arrow-right" height="15" width="15">
+				<ArrowRight />
+			</icon-base>
+		</button>
 	</div>
 </template>
 
 <script>
+import IconBase from "./icons/IconBase";
+import ArrowRight from "./icons/ArrowRight";
 export default {
 	name: "ArtworkPopup",
 	props: {
 		title: String,
+	},
+	components: {
+		IconBase,
+		ArrowRight,
 	},
 };
 </script>
