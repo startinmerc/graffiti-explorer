71380473a3f5af42b8b20590fa45994ca5db26f2
startinmerc <startinmerc@gmail.com>
2020-11-09 11:25:35 +0000

    Add ArtworkPopup component

---
+++ graffiti/graffiti-vue/src/components/ArtworkPopup.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="artwork-popup">
+		<div class="placeholder"></div>
+		<h3>{{ title }}</h3>
+		<p>Artist</p>
+		<button>View Detail</button>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "ArtworkPopup",
+	props: {
+		title: String,
+	},
+};
+</script>
+
+<style lang="scss">
+// Placeholder for artwork image
+.placeholder {
+	height: 50px;
+	// Widen to compensate for .mapbox-popup-content padding
+	width: calc(100% + 20px);
+	margin-left: -10px;
+	margin-top: -10px;
+	// Gradient backgrounds
+	background-image: linear-gradient(132.18deg, #33405c 0%, #afdee4 100%);
+}
+</style>
