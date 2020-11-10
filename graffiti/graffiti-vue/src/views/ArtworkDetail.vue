ef0dcab286b53eb4f5bcd6be25459211161f0405
startinmerc <startinmerc@gmail.com>
2020-11-10 17:43:51 +0000

    Change ArtworkDetail props to data, set on lookup result

--- graffiti/graffiti-vue/src/views/ArtworkDetail.vue
+++ graffiti/graffiti-vue/src/views/ArtworkDetail.vue
@@ -1,10 +1,9 @@
 <template>
 	<main>
-		<img :src="$route.params.photo" alt="artwork"/>
-		<!-- <div class="placeholder"></div> -->
-		<h1>{{ $route.params.title||title }}</h1>
-		<b>{{ $route.params.artist||artist }}</b>
-		<p>{{ $route.params.description||description }}</p>
+		<img :src="$route.params.photo || photo" alt="artwork" />
+		<h1>{{ $route.params.title || title }}</h1>
+		<b>{{ $route.params.artist || artist }}</b>
+		<p>{{ $route.params.description || description }}</p>
 		<router-link to="/map"><button>Back to map</button></router-link>
 	</main>
 </template>
@@ -14,16 +13,24 @@ import { geoData } from "../../data";
 
 export default {
 	name: "ArtworkDetail",
-	props: {
-		title: {type: String, default: "Artwork Title"},
-		artist: {type: String, default: "Artwork Artist"},
-		description: {type: String, default: "Artwork Description"},
+	data: function() {
+		return {
+			title: "Artwork Title",
+			artist: "Artwork Artist",
+			description: "Artwork Description",
+			photo: "Artwork photo",
+		};
+	},
 	mounted() {
 		if (!this.$route.params.title) {
 			try {
 				let res = geoData.data.features.find(
 					(v) => v.properties.id === this.$route.params.id
 				);
+				this.title = res.properties.title;
+				this.description = res.properties.description;
+				this.artist = res.properties.artist;
+				this.photo = res.properties.photos;
 			} catch (error) {
 				console.log(error);
 			}
@@ -38,6 +45,6 @@ img {
 	height: auto;
 }
 .placeholder {
-  height: 50vh;
+	height: 50vh;
 }
 </style>
