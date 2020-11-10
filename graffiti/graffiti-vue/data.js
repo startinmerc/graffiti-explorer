d2cc09f201b1ec83460d50b597ce18c5a25bfefb
startinmerc <startinmerc@gmail.com>
2020-11-10 11:46:43 +0000

    Add artist, id to data

--- graffiti/graffiti-vue/data.js
+++ graffiti/graffiti-vue/data.js
@@ -4,35 +4,40 @@ export const testData = [
 		location: [-1.077706, 53.956799],
 		description:
 			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex in illo quasi, repellat soluta porro architecto? Facilis distinctio natus iste illum quaerat provident debitis, numquam molestiae! Fuga similique officiis laboriosam!",
-		artist: "",
+		artist: "1",
+		id: 1,
 	},
 	{
 		title: "test data 2",
 		location: [-1.086944, 53.957329],
 		description:
 			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex in illo quasi, repellat soluta porro architecto? Facilis distinctio natus iste illum quaerat provident debitis, numquam molestiae! Fuga similique officiis laboriosam!",
-		artist: "",
+		artist: "2",
+		id: 2,
 	},
 	{
 		title: "test data 3",
 		location: [-1.088489, 53.959355],
 		description:
 			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex in illo quasi, repellat soluta porro architecto? Facilis distinctio natus iste illum quaerat provident debitis, numquam molestiae! Fuga similique officiis laboriosam!",
-		artist: "",
+		artist: "1",
+		id: 3,
 	},
 	{
 		title: "test data 4",
 		location: [-1.087631, 53.96402],
 		description:
 			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex in illo quasi, repellat soluta porro architecto? Facilis distinctio natus iste illum quaerat provident debitis, numquam molestiae! Fuga similique officiis laboriosam!",
-		artist: "",
+		artist: "2",
+		id: 4,
 	},
 	{
 		title: "test data 5",
 		location: [-1.082696, 53.964108],
 		description:
 			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex in illo quasi, repellat soluta porro architecto? Facilis distinctio natus iste illum quaerat provident debitis, numquam molestiae! Fuga similique officiis laboriosam!",
-		artist: "",
+		artist: "2",
+		id: 5,
 	},
 ];
 
@@ -46,7 +51,9 @@ function buildJSON(data) {
 		properties: {
 			title: v.title,
 			description: v.description,
-			icon: "rocket",
+			icon: "ArtworkMarker",
+			id: v.id,
+			artist: v.artist,
 		},
 	}));
 	return {
