dd2a8d0b632b79f3700e49005cd58c08e4ec3c71
startinmerc <startinmerc@gmail.com>
2020-11-12 11:59:02 +0000

    Add md+ styling to About

--- graffiti/graffiti-vue/src/views/About.vue
+++ graffiti/graffiti-vue/src/views/About.vue
@@ -1,13 +1,14 @@
 <template>
-	<main>
+	<main id="about" class="main--shrink-wide">
 		<h1>About the app</h1>
 		<div class="padded">
 			<p>
 				Leverage agile frameworks to provide a robust synopsis for high level
 				overviews. Iterative approaches to corporate strategy foster
-				collaborative thinking to further the <router-link to="">overall value</router-link> proposition.
-				Organically grow the holistic world view of disruptive innovation via
-				workplace diversity and empowerment.
+				collaborative thinking to further the
+				<router-link to="">overall value</router-link> proposition. Organically
+				grow the holistic world view of disruptive innovation via workplace
+				diversity and empowerment.
 			</p>
 			<p>
 				Bring to the table win-win survival strategies to ensure proactive
@@ -18,10 +19,13 @@
 			</p>
 			<p>
 				Capitalize on low hanging fruit to identify a ballpark value added
-				activity to beta test. <router-link to="">Override the digital divide with additional
-				clickthroughs from DevOps.</router-link> Nanotechnology immersion along the
-				information highway will close the loop on focusing solely on the bottom
-				line.
+				activity to beta test.
+				<router-link to=""
+					>Override the digital divide with additional clickthroughs from
+					DevOps.</router-link
+				>
+				Nanotechnology immersion along the information highway will close the
+				loop on focusing solely on the bottom line.
 			</p>
 		</div>
 	</main>
@@ -35,7 +39,8 @@ export default {
 
 <style scoped lang="scss">
 main {
-	display: grid;
-	place-items: center;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
 }
 </style>
