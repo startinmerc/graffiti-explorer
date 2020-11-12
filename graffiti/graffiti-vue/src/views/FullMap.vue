c9dfe221a2a3c350558c95c2ab6e9ab4c02f4e39
startinmerc <startinmerc@gmail.com>
2020-11-12 18:42:40 +0000

    Add draft for possible static map image

--- graffiti/graffiti-vue/src/views/FullMap.vue
+++ graffiti/graffiti-vue/src/views/FullMap.vue
@@ -1,6 +1,6 @@
 <template>
 	<main id="full-map">
-		<div id="map-container"></div>
+		<div id="map-container" ref="mapContainer"></div>
 	</main>
 </template>
 
@@ -14,8 +14,16 @@ const ArtworkPopupClass = Vue.extend(ArtworkPopup);
 
 export default {
 	name: "FullMap",
-
 	mounted() {
+		// Generated URL using container's dimentions for image size
+		console.log(
+			`https://api.mapbox.com/styles/v1/mapbox/light-v10/static/-1.0803,53.9583,12,0/${Math.min(
+				this.$refs.mapContainer.clientWidth,
+				1280
+			)}x${this.$refs.mapContainer.clientHeight}?access_token=${
+				process.env.VUE_APP_MAPBOX_TOKEN
+			}`
+		);
 		// Get API token from .env
 		mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_TOKEN;
 
